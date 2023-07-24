9590ce91b1259c24e529f8753cd4ced8834ab61f
0xNika <nika@simelineon.xyz>
2023-07-24 05:18:19 +0200

    nearly fulfilled all sprint 1 goals

--- Sprint 1 Files and Code/SamplePlugin/GagSpeak/Windows/ConfigWindow.cs
+++ Sprint 1 Files and Code/SamplePlugin/GagSpeak/Windows/ConfigWindow.cs
@@ -6,16 +6,17 @@ using ImGuiNET;
 using Gagger;
 namespace Gagger.Windows;
 
+
 public class ConfigWindow : Window, IDisposable
 {
+    private int GagID= 0;
     private Configuration Configuration;
 
     public ConfigWindow(Plugin plugin) : base(
-        "A Wonderful Configuration Window",
-        ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar |
-        ImGuiWindowFlags.NoScrollWithMouse)
+        "Configure your gag",
+        ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoScrollbar |ImGuiWindowFlags.NoScrollWithMouse)
     {
-        this.Size = new Vector2(232, 175);
+        this.Size = new Vector2(250, 125);
         this.SizeCondition = ImGuiCond.Always;
 
         this.Configuration = plugin.Configuration;
@@ -26,19 +27,18 @@ public class ConfigWindow : Window, IDisposable
     public override void Draw()
     {
         // can't ref a property, so use a local copy
-        var enable = this.Configuration.status;
+    /* var enable = this.Configuration.status;   //disbled for the moment cause it's just in an extra text box for now :)
         if (ImGui.Checkbox("Enable", ref enable))
         {
             this.Configuration.status = enable;
             // can save immediately on change, if you don't want to provide a "Save and Close" button
             this.Configuration.Save();
-        }
+        } */
         int power = Configuration.strength;
         if (ImGui.SliderInt("Strength", ref power, (int)0, (int)20))
         {
             Configuration.strength = power;
         }
-        int GagID= 0;
         string[] Name = {"None","Ball Gag","Panel Gag","Plug Gag","Pump Gag","Ring Gag"};
         int[] gagStrength = {0,5,3,5,2,3};
         if (ImGui.BeginCombo("Gag", Name[GagID]))
@@ -46,12 +46,12 @@ public class ConfigWindow : Window, IDisposable
             int i = 0;
             foreach (string gag in Name)
             {
-                i++;
-                if (ImGui.Selectable(gag, gag == Name[GagID]) && gag != Name[GagID])
+                if (ImGui.Selectable(gag, gag == Name[GagID]))
                 {
                     GagID = i;
                     Configuration.strength = gagStrength[i];
-                } 
+                }
+                i++; 
             }
             ImGui.EndCombo();
         }
